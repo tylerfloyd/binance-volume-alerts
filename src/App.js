5c7dc1d1fcd4315e9b86cee918459186f2a82161
unknown <tylerbfloyd@gmail.com>
2018-03-24 23:47:37 -0700

    added basic analytics script

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Container, Card } from 'semantic-ui-react';
 import _ from 'lodash';
+import ReactGA from 'react-ga';
 
 import './App.css';
 
@@ -12,52 +13,52 @@ import Coin from './components/Card';
 import FilterMenu from './components/FilterMenu';
 
 class App extends Component {
-  componentWillMount() {
-    /**
-     * When we know we are good to go,
-     * call into Firebase to get the coins
-     */
-    this.props.dispatch.getCoins();
-  }
+	componentWillMount() {
+		/**
+		 * When we know we are good to go,
+		 * call into Firebase to get the coins
+		 */
+		this.props.dispatch.getCoins();
+		ReactGA.initialize('UA-116367609-1');
+		ReactGA.ga('send', 'pageview', 'home');
+	}
 
-  /**
-   * Component function that will build out the card elements
-   */
-  renderCoins = (data, coin) => {
-    if (data.updated) {
-      return <Coin key={data.coin} data={data} coin={data.coin} />;
-    } else {
-      return null;
-    }
-  };
+	/**
+	 * Component function that will build out the card elements
+	 */
+	renderCoins = (data, coin) => {
+		if (data.updated) {
+			return <Coin key={data.coin} data={data} coin={data.coin} />;
+		} else {
+			return null;
+		}
+	};
 
-  render() {
-    const { coins } = this.props;
+	render() {
+		const { coins } = this.props;
 
-    return (
-      <div className="App">
-        <Container>
-          <FilterMenu />
-        </Container>
-        <Container>
-          <Card.Group centered>
-            {_.map(coins, (data, coin) => this.renderCoins(data, coin))}
-          </Card.Group>
-        </Container>
-      </div>
-    );
-  }
+		return (
+			<div className="App">
+				<Container>
+					<FilterMenu />
+				</Container>
+				<Container>
+					<Card.Group centered>{_.map(coins, (data, coin) => this.renderCoins(data, coin))}</Card.Group>
+				</Container>
+			</div>
+		);
+	}
 }
 const mapStateToProps = (state, ownProps) => {
-  return state;
+	return state;
 };
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ getCoins }, dispatch);
+	return bindActionCreators({ getCoins }, dispatch);
 };
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => {
-  return { ...ownProps, ...stateProps, dispatch: dispatchProps };
+	return { ...ownProps, ...stateProps, dispatch: dispatchProps };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(App);
