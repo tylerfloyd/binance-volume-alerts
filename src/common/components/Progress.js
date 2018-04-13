f00e5fed1800ce260955d807d174ee7a19fd3ed5
unknown <tylerbfloyd@gmail.com>
2018-04-13 08:14:15 -0700

    updated progress component to leverage reselector

--- src/common/components/Progress.js
+++ src/common/components/Progress.js
@@ -1,37 +1,20 @@
 import React, { Component } from 'react';
 import { Progress } from 'semantic-ui-react';
 
+import { determineStatus } from '../selectors/index';
+
 export default class coinProgress extends Component {
 	/**
 	 * TODO: memoize this function for reuseability and performance
 	 */
-	getPercent = (count, netVol) => {
+	getCoinStatus = (count, netVol) => {
 		const positiveVolume = netVol > 0;
-		let result = {
-			percent: 0,
-			color: 'red'
-		};
-
-		if (count > 0 && count < 3 && positiveVolume) {
-			result.percent = 25;
-			result.color = 'orange';
-		} else if (count > 2 && count < 5 && positiveVolume) {
-			result.percent = 50;
-			result.color = 'yellow';
-		} else if ((count > 4 && count < 10 && positiveVolume) || positiveVolume) {
-			result.percent = 75;
-			result.color = 'green';
-		} else {
-			result.percent = 100;
-			result.color = 'red';
-		}
-
-		return result;
+		return determineStatus({ count, positiveVolume });
 	};
 
 	render() {
 		const { count, netVol, size = 'medium' } = this.props;
-		const progressAttributes = this.getPercent(count, netVol);
+		const progressAttributes = this.getCoinStatus(count, netVol);
 		return (
 			<div>
 				<Progress percent={progressAttributes.percent} color={progressAttributes.color} label={count} size={size} />
